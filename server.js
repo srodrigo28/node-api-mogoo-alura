d4080de03fa0e119e64993b027aa878db3b0e5e6
Sebastiao <rodrigoexer1@gmail.com>
2023-03-23 22:26:31 -0300

    updateAdErrorWith

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 import app from './src/app.js'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
-    console.log(`Server rodando em http://localhost:${port}`)
+  console.log(`Servidor escutando em http://localhost:${port}`)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/app.js
+++ src/app.js
@@ -1,28 +1,48 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
-const livros = [ 
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobiit"}
-]
+app.use(express.json())
 
-const jogos = [
-    {id: 1, "titulo": "Street Fighter III", "ano": 2000},
-    {id: 2, "titulo": "Street Fighter IV", "ano": 2005},
-    {id: 3, "titulo": "Street Fighter V", "ano": 2010}
+const livros = [
+  {id: 1, "titulo": "Senhor dos Aneis"},
+  {id: 2, "titulo": "O Hobiit"}
 ]
 
 app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
+  res.status(200).send('Curso de Node');
+}) 
 
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
+  res.status(200).json(livros)
 })
 
-app.get('/jogos', (req, res) => {
-    res.status(200).json(jogos)
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
 })
+
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('Livro foi cadastrado com sucesso')
+})
+
+app.put('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+})
+
+app.delete('/livros/:id', (req, res) => {
+  let {id} = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} removido com sucesso`);
+})
+
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
